46fb0139dfb8f59a9804e1371d7d3a5d98e02934
julakshah <julakshah@gmail.com>
2025-03-24 17:40:34 -0400

    simple 2 dof implementation for 5 dof

--- arm_models.py
+++ arm_models.py
@@ -940,8 +940,33 @@ class FiveDOFRobot:
             soln: Optional parameter for multiple solutions (not implemented).
         """
         ########################################
+        # Solve theta 1
+        x, y = EE.x, EE.y
+        theta_1 = np.arctan2(y, x)
+        self.theta[0] = theta_1
 
-        # insert your code here
+        # Find rotation of EE matrix
+        EE_euler = [EE.rotx, EE.roty, EE.rotz]
+        EE_rot = euler_to_rotm(EE_euler)
+
+        # Find Position of joint 3 in base frame
+        P_EE = np.array([EE.x, EE.y, EE.z])
+        k = np.array([0, 0, 1])
+        P_3 = P_EE - (self.l4 + self.l5) * EE_rot @ k
+
+        # Solve decoupled kinematic problem in frame 1 for theta 2 & 3
+        P3_x, P3_y, P3_z = P_3[0], P_3[1], P_3[2]
+        L = np.sqrt(P3_x**2 + P3_y**2 + (P3_z - self.l1) ** 2)  # solve for L in 3D
+        delta_x = np.sqrt(P3_x**2 + P3_y**2)  # solve for x displacement from 1 to 3
+        delta_z = P3_z - self.l1  # solve for z displacement from 1 to 3
+        alpha = np.arctan2(delta_z, delta_x)
+        phi = np.arccos((self.l2**2 + self.l3**2 - L**2) / (2 * self.l2 * self.l3))
+        theta_3 = np.pi = phi
+        gamma = self.l3 * sin(theta_3)
+        beta = np.arcsin(gamma / L)
+        theta_2 = alpha - beta
+        self.theta[1], self.theta[2] = theta_2, theta_3
+        print(f"theta 1, 2, 3 are {theta_1}, {theta_2}, {theta_3}")
 
         ########################################
 
